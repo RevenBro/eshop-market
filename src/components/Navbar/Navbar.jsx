3080d428877b44f4b0ad6880948acd3f45304ea5
RevenBro <saidakbarov077@gmail.com>
2024-12-21 21:18:23 +0500

    Header section done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { IoMdSearch } from 'react-icons/io'
+import DarkMode from '../DarkMode'
+import { FaCaretDown } from 'react-icons/fa'
 
 const Navbar = () => {
 
@@ -25,6 +27,29 @@ const Navbar = () => {
             link: "/#blogs"
         },
     ]
+
+    const DropdownLinks = [
+        {
+            id: 1,
+            name: "Trending Products",
+            link: "/#"
+        },
+        {
+            id: 2,
+            name: "Best Selling",
+            link: "/#"
+        },
+        {
+            id: 3,
+            name: "Top Rated",
+            link: "/#"
+        },
+        {
+            id: 4,
+            name: "Special Offers",
+            link: "/#"
+        },
+    ]
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40'>
         <div className='py-4'>
@@ -41,6 +66,27 @@ const Navbar = () => {
                                     {item.name}</a>
                                 </li>
                             ))}
+
+                            <li className='relative cursor-pointer group'>
+                                <a href="#" className='flex items-center gap-[2px] font-semibold text-gray-500 py-2 dark:hover:text-white'>
+                                    Quick Links
+                                    <span>
+                                        <FaCaretDown className='group-hover:rotate-180 duration-300'/>
+                                    </span>
+                                </a>
+
+                                <div className='absolute z-[999] hidden group-hover:block w-[200px] rounded-md bg-white dark:bg-gray-900 p-2 dark:text-white shadow-md'>
+                                    <ul className='space-y-2'>
+                                        {DropdownLinks.map((item, index) => (
+                                             <li key={index}>
+                                                 <a href={item.link} className='text-gray-500 hover:text-black dark:hover:text-white duration-200 p-2 hover:bg-primary/20 inline-block w-full rounded-md font-semibold'>
+                                                     {" "}
+                                                     {item.name}</a>
+                                             </li>
+                                        ))}
+                                    </ul>
+                                </div>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -51,7 +97,9 @@ const Navbar = () => {
                         <IoMdSearch className='text-xl text-gray-600 dark:text-gray-400 absolute top-1/2 -translate-y-1/2 right-3'/>
                     </div>
                     
-                    <div></div>
+                    <div>
+                        <DarkMode/>
+                    </div>
                 </div>
             </div>
         </div>
