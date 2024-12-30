8d70cab55e5e8ecd35b68664bc95e7a0ea89231f
RevenBro <saidakbarov077@gmail.com>
2024-12-30 18:51:34 +0500

    Services section started

--- src/components/Category/Category2.jsx
+++ src/components/Category/Category2.jsx
@@ -9,40 +9,39 @@ const Category = () => {
     <div className='py-8'>
       <div className='container'>
         <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8'>
-          <div className='col-span-2 py-10 pl-5 bg-gradient-to-br from-primary to-primary/90 text-white rounded-3xl relative h-[320px] flex items-end '>
+          <div className='col-span-2 py-10 pl-5 bg-gradient-to-br from-gray-400/90 to-gray-100 text-white rounded-3xl relative h-[320px] flex items-end '>
             <div >
               <div className='mb-4'>
                 <p className='mb-[2px] text-white'>Enjoy</p>
                 <p className='text-2xl font-semibold mb-[2px]'>With</p>
                 <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Laptop</p>
-                <Button text="Browse" bgColor="bg-white" textColor="text-primary"/>
+                <Button text="Browse" bgColor="bg-primary" textColor="text-white"/>
               </div>
             </div>
             <img className='w-[250px] absolute top-1/2 -translate-y-1/2 -right-0' src={Image1} alt="Laptops" />
           </div>
 
-          <div className='py-10 pl-5 bg-gradient-to-br from-black/90 to-black/70 text-white rounded-3xl relative h-[320px] flex items-end '>
+          <div className='py-10 pl-5 bg-gradient-to-br from-brandGreen/90 to-brandGreen/90 text-white rounded-3xl relative h-[320px] flex items-start'>
             <div >
               <div className='mb-4'>
-                <p className='mb-[2px] text-gray-400'>Enjoy</p>
+                <p className='mb-[2px] text-white'>Enjoy</p>
                 <p className='text-2xl font-semibold mb-[2px]'>With</p>
                 <p className='text-4xl xl:text-5xl font-bold opacity-20 mb-2'>Earphone</p>
-                <Button text="Browse" bgColor="bg-primary" textColor="text-white"/>
+                <Button text="Browse" bgColor="bg-white" textColor="text-brandGreen"/>
               </div>
             </div>
             <img className='w-[320px] absolute bottom-0' src={Image2} alt="Earphones" />
           </div>
 
-          <div className='py-10 pl-5 bg-gradient-to-br from-brandYellow to-brandYellow/90 text-white rounded-3xl relative h-[320px] flex items-end '>
+          <div className='py-10 pl-5 bg-gradient-to-br from-brandBlue to-brandBlue/90 text-white rounded-3xl relative h-[320px] flex items-start '>
             <div >
               <div className='mb-4'>
                 <p className='mb-[2px] text-white'>Enjoy</p>
                 <p className='text-2xl font-semibold mb-[2px]'>With</p>
                 <p className='text-4xl xl:text-5xl font-bold opacity-40 mb-2'>Gadget</p>
-                <Button text="Browse" bgColor="bg-white" textColor="text-brandYellow"/>
+                <Button text="Browse" bgColor="bg-white" textColor="text-brandBlue"/>
               </div>
             </div>
-            <img className='w-[320px] absolute -right-4 lg:top-[40px]' src={Image3} alt="Smart watches" />
           </div>
 
   
