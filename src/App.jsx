f8141bd60472b9596c4b5c92b0b0216bd66ef3ca
RevenBro <saidakbarov077@gmail.com>
2025-01-03 00:34:51 +0500

    Banner section completed

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ import Category2 from './components/Category/Category2'
 import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Services from './components/Services/Services'
-import Headphone from './assets/Images/headphone.png'
 import Products from './components/Products/Products'
+
+import Headphone from './assets/Images/headphone.png'
+import smartwatch2 from './assets/Images/smartwatch2-removebg-preview.png'
 
 function App() {
 
@@ -19,7 +21,18 @@ function App() {
     title3: "Winter Sale",
     title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem praesentium voluptate cum libero.",
     bgColor: "#f42c37",
-}
+  };
+
+  const BannerData2 = {
+    discount: "40% OFF",
+    title: "Happy Hours",
+    date: "14 Jan to 28 Jan",
+    image: smartwatch2,
+    title2: "Smart Solo",
+    title3: "Spring Sale",
+    title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem praesentium voluptate cum libero.",
+    bgColor: "#2dcc6f",
+  }
 
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
@@ -30,6 +43,7 @@ function App() {
       <Services/>
       <Banner data={BannerData}/>
       <Products/>
+      <Banner data={BannerData2}/>
     </div>
   )
 }
