95b43b6a2a6722709fb90ba7af7e14e12b79775c
RevenBro <saidakbarov077@gmail.com>
2025-01-04 20:19:50 +0500

    Some bugs fixed and Hero section animated

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import {useEffect} from 'react'
 import './App.css'
 import Banner from './components/Banner/Banner'
 import Category from './components/Category/Category'
@@ -6,12 +7,15 @@ import Hero from './components/Hero/Hero'
 import Navbar from './components/Navbar/Navbar'
 import Services from './components/Services/Services'
 import Products from './components/Products/Products'
-
-import Headphone from './assets/Images/headphone.png'
-import smartwatch2 from './assets/Images/smartwatch2-removebg-preview.png'
 import Blogs from './components/Blogs/Blogs'
 import Partners from './components/Partners/Partners'
 import Footer from './components/Footer/Footer'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import Headphone from './assets/Images/headphone.png'
+import smartwatch2 from './assets/Images/smartwatch2-removebg-preview.png'
+
 
 function App() {
 
@@ -36,6 +40,16 @@ function App() {
     title4: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem praesentium voluptate cum libero.",
     bgColor: "#2dcc6f",
   }
+
+  useEffect(() => {
+    AOS.init({
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+      offset: 100
+    });
+    AOS.refresh();
+  }, [])
 
   return (
     <div className='bg-white dark:bg-gray-900 dark:text-white duration-200 overflow-hidden'>
